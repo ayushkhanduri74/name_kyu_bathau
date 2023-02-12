db9ef5426e3ac86543b02e2f4e77f0edb56c9521
Ayush Khanduri <113684157+ayushkhanduri78@users.noreply.github.com>
2023-02-12 20:13:47 +0530

    Update startmsg.py

--- plugins/startmsg.py
+++ plugins/startmsg.py
@@ -1,67 +1,66 @@
 class Translation(object):
     START_TEXT = """Hi {} 👋
 
-I'm Url Uploader Bot 🚀
+I'm Advance Url Uploader Bot 🚀
 
 <b>Permanent Thumbnail Support💯.</b>
 
-<i>Send me a direct link and I will upload it to telegram as a file/video.</i>
+<i>sᴇɴᴅ ᴍᴇ ᴀ ᴅɪʀᴇᴄᴛ ʟɪɴᴋ ᴀɴᴅ ɪ ᴡɪʟʟ ᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀs ᴀ ғɪʟᴇ/ᴠɪᴅᴇᴏ.</i>
 
 Click /help for more details...."""
-    ADD_CAPTION_HELP = """Select an uploaded file/video or forward me <b>Any Telegram File</b> and just write the text you want to be on the file <b>as a reply to the file</b> and the text you wrote will be attached as the caption! 🤩
+    ADD_CAPTION_HELP = """sᴇʟᴇᴄᴛ ᴀɴ ᴜᴘʟᴏᴀᴅᴇᴅ ғɪʟᴇ/ᴠɪᴅᴇᴏ ᴏʀ ғᴏʀᴡᴀʀᴅ ᴍᴇ <b> ᴀɴʏ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ</b> ᴀɴᴅ ᴊᴜsᴛ ᴡʀɪᴛᴇ ᴛʜᴇ ᴛᴇxᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙᴇ ᴏɴ ᴛʜᴇ ғɪʟᴇ <b>ᴀs ᴀ ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ғɪʟᴇ</b>ᴀɴᴅ ᴛʜᴇ ᴛᴇxᴛ ʏᴏᴜ ᴡʀᴏᴛᴇ ᴡɪʟʟ ʙᴇ ᴀᴛᴛᴀᴄʜᴇᴅ ᴀs ᴛʜᴇ ᴄᴀᴘᴛɪᴏɴ! 🤩
     
 Example: <a href='https://te.legra.ph/file/ecf5297246c5fb574d1a0.jpg'>See This!</a> 👇"""
 
-    INCORRECT_REQUEST = """Please make sure you submit your request correctly."""
-    WAIT_PROCESS_FINISH = """Please wait for your current file to finish downloading before sending more links!
+    INCORRECT_REQUEST = """ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ sᴜʀᴇ ʏᴏᴜ sᴜʙᴍɪᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴄᴏʀʀᴇᴄᴛʟʏ."""
+    WAIT_PROCESS_FINISH = """ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ғɪʟᴇ ᴛᴏ ғɪɴɪsʜ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ʙᴇғᴏʀᴇ sᴇɴᴅɪɴɢ ᴍᴏʀᴇ ʟɪɴᴋs!
 
-Or use /cancel to terminate incomplete processes."""
-    PROCESS_CANCELLED = """File upload cancelled
-You can now send a new URL."""
-    NO_PROCESS_FOUND = """🤷‍♂️ No pending uploads were found. You can upload files by sending a link now!"""
+ᴏʀ ᴜsᴇ /ᴄᴀɴᴄᴇʟ ᴛᴏ ᴛᴇʀᴍɪɴᴀᴛᴇ ɪɴᴄᴏᴍᴘʟᴇᴛᴇ ᴘʀᴏᴄᴇssᴇs."""
+    PROCESS_CANCELLED = """ғɪʟᴇ ᴜᴘʟᴏᴀᴅ ᴄᴀɴᴄᴇʟʟᴇᴅ
+ʏᴏᴜ ᴄᴀɴ ɴᴏᴡ sᴇɴᴅ ᴀ ɴᴇᴡ ᴜʀʟ."""
+    NO_PROCESS_FOUND = """🤷‍♂️ ɴᴏ ᴘᴇɴᴅɪɴɢ ᴜᴘʟᴏᴀᴅs ᴡᴇʀᴇ ғᴏᴜɴᴅ. ʏᴏᴜ ᴄᴀɴ ᴜᴘʟᴏᴀᴅ ғɪʟᴇs ʙʏ sᴇɴᴅɪɴɢ ᴀ ʟɪɴᴋ ɴᴏᴡ!"""
     FORMAT_SELECTION = "👇𝗦𝗲𝗹𝗲𝗰𝘁 𝗔𝗻𝗱 𝗖𝗵𝗼𝘀𝗲 𝗬𝗼𝘂𝗿 𝗙𝗼𝗿𝗺𝗮𝘁👇"
     SET_CUSTOM_USERNAME_PASSWORD = """\n👮‍♂ Powered By :</b> @LISA_FAN_LK"""
     DOWNLOAD_START = "📥 DOWNLOADING..."
     UPLOAD_START = "📤 UPLOADING..."
-    RCHD_TG_API_LIMIT = "<b>Downloaded in:</b> {} seconds.\n<b>Detected file size:</b> {}.\n\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations 😕."
+    RCHD_TG_API_LIMIT = "<b>Downloaded in:</b> {} seconds.\n<b>Detected file size:</b> {}.\n\nsᴏʀʀʏ. ʙᴜᴛ, ɪ ᴄᴀɴɴᴏᴛ ᴜᴘʟᴏᴀᴅ ғɪʟᴇs ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ 𝟸ɢʙ ᴅᴜᴇ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴘɪ ʟɪᴍɪᴛᴀᴛɪᴏɴs 😕."
     #AFTER_SUCCESSFUL_UPLOAD_MSG = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰\n\n@NT_BOT_CHANNEL"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰\n\n@skmovieslinks"
     SAVED_CUSTOM_THUMB_NAIL = "Save Your Thumbnail ✅."
     DEL_ETED_CUSTOM_THUMB_NAIL = " Delete Your Thumbnail ✅."
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = """<b>I think you have entered an unaccessible url or a private url.</b>
-<i>Go check if you can access the content in the url from your browser first!</i>
+    NO_VOID_FORMAT_FOUND = """<b>ɪ ᴛʜɪɴᴋ ʏᴏᴜ ʜᴀᴠᴇ ᴇɴᴛᴇʀᴇᴅ ᴀɴ ᴜɴᴀᴄᴄᴇssɪʙʟᴇ ᴜʀʟ ᴏʀ ᴀ ᴘʀɪᴠᴀᴛᴇ ᴜʀʟ.</b>
+<i>ɢᴏ ᴄʜᴇᴄᴋ ɪғ ʏᴏᴜ ᴄᴀɴ ᴀᴄᴄᴇss ᴛʜᴇ ᴄᴏɴᴛᴇɴᴛ ɪɴ ᴛʜᴇ ᴜʀʟ ғʀᴏᴍ ʏᴏᴜʀ ʙʀᴏᴡsᴇʀ ғɪʀsᴛ!</i>
 
 <b>YouTubeDL</b> said: {}"""
-    HELP_USER = """<b>How to Use Me?</b> 🤔
+    HELP_USER = """<b>ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ</b> 🤔
 
-1. Send your URL link
+1. 🇸‌🇪‌🇳‌🇩‌ 🇾‌🇴‌🇺‌🇷‌ 🇺‌🇷‌🇱‌ 🇱‌🇮‌🇳‌🇰‌
 
-2. Send thumbnail photo to save it permanently.
+2. 🇸‌🇪‌🇳‌🇩‌ 🇹‌🇭‌🇺‌🇲‌🇧‌🇳‌🇦‌🇮‌🇱‌ 🇵‌🇭‌🇴‌🇹‌🇴‌ 🇹‌🇴‌ 🇸‌🇦‌🇻‌🇪‌ 🇮‌🇹‌ 🇵‌🇪‌🇷‌🇲‌🇦‌🇳‌🇪‌🇳‌🇹‌🇱‌🇾‌.
 
-3. Select the desired option
+3. 🇸‌🇪‌🇱‌🇪‌🇨‌🇹‌ 🇹‌🇭‌🇪‌ 🇩‌🇪‌🇸‌🇮‌🇷‌🇪‌🇩‌ 🇴‌🇵‌🇹‌🇮‌🇴‌🇳‌
 
-4. Send /delthumbnail To Delete Your Thumbnail.
+4. 🇸‌🇪‌🇳‌🇩‌ /🇩‌🇪‌🇱‌🇹‌🇭‌🇺‌🇲‌🇧‌🇳‌🇦‌🇮‌🇱‌ 🇹‌🇴‌ 🇩‌🇪‌🇱‌🇪‌🇹‌🇪‌ 🇾‌🇴‌🇺‌🇷‌ 🇹‌🇭‌🇺‌🇲‌🇧‌🇳‌🇦‌🇮‌🇱‌.
 
-5.  Use /caption to Set caption as Reply to Media
+5.  🇺‌🇸‌🇪‌ /🇨‌🇦‌🇵‌🇹‌🇮‌🇴‌🇳‌ 🇹‌🇴‌ 🇸‌🇪‌🇹‌ 🇨‌🇦‌🇵‌🇹‌🇮‌🇴‌🇳‌ 🇦‌🇸‌ 🇷‌🇪‌🇵‌🇱‌🇾‌ 🇹‌🇴‌ 🇲‌🇪‌🇩‌🇮‌🇦‌
 
-6. Comments /about /viewthumbnail /info
+6. 🇨‌🇴‌🇲‌🇲‌🇪‌🇳‌🇹‌🇸‌ /🇦‌🇧‌🇴‌🇺‌🇹‌ /🇻‌🇮‌🇪‌🇼‌🇹‌🇭‌🇺‌🇲‌🇧‌🇳‌🇦‌🇮‌🇱‌ /🇮‌🇳‌🇫‌🇴‌
 """
-    ABOUT_TEXT = """<b>🔘 My Name :</b> URL Uploader Bot V2 🚀
+    ABOUT_TEXT = """<b>🔘 My Name :</b> URL Uploader Bot🚀
 
-<b>🔘 Channel :</b> <a href="https://t.me/NT_BOT_CHANNEL">NT BOT</a>
+<b>🔘 Channel :</b> <a href="https://t.me/skmovieslinks">NT BOT</a>
 
-<b>🔘 Source :</b> <a href="https://github.com/LISA-KOREA/UPLOADER-BOT-V2">Click Here</a>
+<b>🔘 Source :</b> <a href="https://te.legra.ph/file/2184b7d4cd6fa1cfc8ac4.mp4">ᴄʟɪᴄᴋ ᴍᴀᴛ ᴋᴀʀ ʟᴀᴜᴅᴇ</a>
 
 <b>🔘 Language :</b> <a href="https://www.python.org/">Python 3.10.8</a>
 
 <b>🔘 Framework :</b> <a href="https://docs.pyrogram.org/">Pyrogram 1.4.16</a>
 
-<b>🔘 Creater :</b> @LISA_FAN_LK"""
 
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Send your thumbnail pic to generate custom thumbail."
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-    CANCEL_STR = "❌ Cancelled ❌"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "sᴇɴᴅ ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ ᴘɪᴄ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙᴀɪʟ."
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "ᴍᴇᴅɪᴀ ᴀʟʙᴜᴍ sʜᴏᴜʟᴅ ᴄᴏɴᴛᴀɪɴ ᴏɴʟʏ ᴛᴡᴏ ᴘʜᴏᴛᴏs. ᴘʟᴇᴀsᴇ ʀᴇ-sᴇɴᴅ ᴛʜᴇ ᴍᴇᴅɪᴀ ᴀʟʙᴜᴍ, ᴀɴᴅ ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ, ᴏʀ sᴇɴᴅ ᴏɴʟʏ ᴛᴡᴏ ᴘʜᴏᴛᴏs ɪɴ ᴀɴ ᴀʟʙᴜᴍ."
+    CANCEL_STR = "❌ ℂ𝕒𝕟𝕔𝕖𝕝𝕝𝕖𝕕 ❌"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds."
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
